720de9adbc66c8c841241aa82876b0a5a5c77ee3
Tanya Landsman <tlandsman@activecampaign.com>
2024-10-17 11:20:22 -0500

    Initial Commit

---
+++ BarCalc Watch App/BarCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCalcApp.swift
+//  BarCalc Watch App
+//
+//  Created by Tanya Landsman on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCalc_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCalc Watch AppTests/BarCalc_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarCalc_Watch_AppTests.swift
+//  BarCalc Watch AppTests
+//
+//  Created by Tanya Landsman on 10/17/24.
+//
+
+import Testing
+@testable import BarCalc_Watch_App
+
+struct BarCalc_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
