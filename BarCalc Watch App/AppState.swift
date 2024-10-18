5dd8d001606d6a5e45d1d2c20c39344ccbabbae6
Tanya Landsman <tlandsman@activecampaign.com>
2024-10-18 18:03:55 -0500

    Set up

---
+++ BarCalc Watch App/AppState.swift
@@ -0,0 +1,88 @@
+import Combine
+
+
+struct AppState {
+    var sidePlateWeight: Double
+    let barWeight: Double
+   
+    var plateArray: [PlateState]
+    
+    var total: Int {
+        return Int(sidePlateWeight * 2 + barWeight)
+    }
+}
+
+enum Action {
+    case weightTapped(Double)
+    case clearTapped
+}
+
+func reducer(state: inout AppState, action: Action) {
+    switch action {
+    case .weightTapped(let weight):
+        state.sidePlateWeight = state.sidePlateWeight + weight
+        print(">>> total plateWeight: \(state.sidePlateWeight)")
+        print(">>> total: \(state.total)")
+       
+    case .clearTapped:
+        state.sidePlateWeight = 0
+    }
+}
+    
+final class AppStore: ObservableObject {
+    @Published var state: AppState
+    
+    init(state: AppState = AppState(sidePlateWeight: 0, barWeight: 35, plateArray: PlateStateFactory().createPlateStates()) ) {
+        self.state = state
+    }
+    
+    func onAction(_ action: Action) {
+        reducer(state: &state, action: action)
+    }
+}
+
+
+struct PlateState: Hashable, Identifiable {
+    let weight: Double
+    var count: Int
+    var id: Double {
+        return weight
+    }
+    
+    init(weight: Double, count: Int = 0) {
+        self.weight = weight
+        self.count = count
+    }
+}
+
+
+enum PlateAction {
+    case counterTapped
+}
+
+
+//protocol ActionProtocol {}
+//protocol EventProtocol {}
+//protocol StateProtocol {}
+//
+//protocol Reducer {
+//    func reduce(state: StateProtocol, action: ActionProtocol) -> StateProtocol
+//}
+
+func reduce(state: inout PlateState, action: PlateAction) {
+    switch action {
+    case .counterTapped:
+        state.count = state.count + 1
+    }
+}
+
+
+struct PlateStateFactory {
+    private let weights = [2.5, 5, 10, 15, 25, 30]
+    func createPlateStates() -> [PlateState] {
+        return weights.map { PlateState(weight: $0)}
+    }
+}
+
+
+
--- BarCalc Watch App/ContentView.swift
+++ BarCalc Watch App/ContentView.swift
@@ -8,17 +8,153 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var store: AppStore
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WeightGridView(store: store)
+            
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(store: AppStore( state: AppState(sidePlateWeight: 0, barWeight: 35, plateArray: PlateStateFactory().createPlateStates())))
+}
+
+
+struct WeightGridView: View {
+    @ObservedObject var store: AppStore
+    // Define the grid layout with columns
+    let columns = [
+        GridItem(.flexible()), // Flexible column adjusts based on available space
+        GridItem(.flexible()), // Second flexible column
+        GridItem(.flexible()),
+   
+    ]
+   // let weights = [2.5, 5, 10, 15, 25, 30]
+   
+    
+    
+    var body: some View {
+        ScrollView {
+           
+            HStack {
+                Text("Total: \(store.state.total)")
+                    .font(.title3)
+                  //  .padding(10)
+          
+                Image(systemName: "xmark.circle")
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        store.onAction(.clearTapped)
+                    }
+
+            }
+            LazyVGrid(columns: columns) {
+                ForEach(store.state.plateArray) { state in
+                    PlateButton(weight: state.weight) {_ in
+                        store.onAction(.weightTapped(state.weight))
+                    }
+                    
+                }
+            }
+            .background(Color.clear)
+            
+  
+        }
+    }
+}
+
+
+
+
+struct PlateButton: View {
+    let weight: Double
+    let buttonAction:(Double) -> Void
+    let counter: Int = 0
+    var displayWeight: String {
+        if (weight == 2.5) {
+            return "\(weight)"
+        } else {
+            return ("\(Int(weight))")
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            PlateView(weight: "\(displayWeight)")
+                .onTapGesture {
+                    buttonAction(weight)
+                }
+            if counter > 1 {
+                BadgeView(counter: counter)
+                    .offset(x: 24, y: -25) // Position the badge at the top-right
+            }
+           
+        }
+        .padding(4)
+      
+    }
+}
+
+struct PlateButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PlateButton(weight: 5, buttonAction: {_ in })
+    }
+}
+
+
+struct PlateView: View {
+    var weight: String
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.gray) // Fill the circle with color
+                .frame(width: 60, height: 60) // Define the size of the circle
+                .overlay(content: {
+                    Circle()
+                        .fill(Color.black)
+                        .frame(width: 34, height: 34)
+                        .overlay {
+                            Circle().stroke(Color.white, lineWidth: 1)
+                                .shadow(color: Color.black, radius: 2.0)
+                        }
+                        
+                })
+                .overlay {
+                    Circle().stroke(Color.white, lineWidth: 3)
+                }
+                .shadow(color: Color.black, radius: 1.0)
+            
+            
+            Text(weight)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .font(.footnote)
+        }
+       
+            
+            
+          
+           
+        
+    }
+}
+
+struct PlateView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlateView(weight: "5")
+    }
+}
+
+
+struct BadgeView: View {
+    var counter: Int
+    
+    var body: some View {
+        Text("/(counter)")
+            .font(.caption)
+            .foregroundColor(.white)
+            .frame(width: 20, height: 20)
+            .background(Color.blue)
+            .clipShape(Circle())
+    }
 }
